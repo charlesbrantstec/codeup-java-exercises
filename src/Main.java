f1e9a047e85b15d709d5cb5ce550870c3020ca28
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-08-10 09:03:08 -0500

    java

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args){
+//        System.out.println("Hello, World");
+//        int myFavoriteNumber = 29;
+//        String myString = "I want to play WoW right now";
+//        char myString = 'W';
+//        float myString = 3.4159;
+//        System.out.println(myString);
+//        float myNumber = (float) 3.14;
+//        System.out.print(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String class = "yo";
+//        System.out.println(class);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        int x = 4;
+        x = x + 5;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y = y * x;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        y -= x/y;
+
+
+    }
+
+}
